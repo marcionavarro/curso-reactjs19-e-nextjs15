65a7a65e22d117ffea53c2b5ac1885e30f0bab45
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-09-07 18:43:13 -0300

    [chronos-pomodoro] 40. Criando a página Home e um template para todas as outras páginas

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,51 +1,7 @@
-import { Container } from './components/Container';
-import { CountDown } from './components/CountDown';
-import { Cycles } from './components/Cycles';
-import { DefaultInput } from './components/DefaultInput';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-
+import { Home } from './pages/Home';
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form action='' className='form'>
-          <div className='formRow'>
-            <DefaultInput
-              id='meuinput'
-              labelText='task'
-              type='text'
-              placeholder='Digite algo'
-            />
-          </div>
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <button>Enviar</button>
-          </div>
-        </form>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
--- chronos-pomodoro/src/components/Menu/index.tsx
+++ chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,20 +1,75 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
+
+type AvalilableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvalilableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvalilableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    evento: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    evento.preventDefault();
+    setTheme(prevTheme => {
+      return prevTheme === 'dark' ? 'light' : 'dark';
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a href='' className={styles.menuLink}>
+      <a
+        href=''
+        className={styles.menuLink}
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
         <HouseIcon />
       </a>
-      <a href='' className={styles.menuLink}>
+      <a
+        href=''
+        className={styles.menuLink}
+        aria-label='Ver Historico'
+        title='Ver Historico'
+      >
         <HistoryIcon />
       </a>
-      <a href='' className={styles.menuLink}>
+      <a
+        href=''
+        className={styles.menuLink}
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a href='' className={styles.menuLink}>
-        <SunIcon />
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Mudar tema'
+        title='Mudar tema'
+        onClick={event => handleThemeChange(event)}
+      >
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
