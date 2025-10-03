517dcb575f1ec28b3528ff8545a57042b662d035
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-10-03 19:57:48 -0300

    53. useRef + Ref para não causar re-renderizações nos componentes

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -2,31 +2,27 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { useTaskContext } from '../../contexts/TaskContext';
+import { useRef /* useState */ } from 'react';
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  //const [taskName, setTaskName] = useState('');
+  const taskNameInput = useRef<HTMLInputElement | null>(null);
 
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        formattedSecondsRemaining: '21:00',
-      };
-    });
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
   }
 
   return (
     <>
-      <form action='' className='form'>
-        <button type='button' onClick={handleClick}>
-          clicar
-        </button>
+      <form action='' className='form' onSubmit={handleCreateNewTask}>
         <div className='formRow'>
           <DefaultInput
             id='meuinput'
             labelText='task'
             type='text'
+            /* value={taskName}
+            onChange={e => setTaskName(e.target.value)} */
+            ref={taskNameInput}
             placeholder='Digite algo'
           />
         </div>
